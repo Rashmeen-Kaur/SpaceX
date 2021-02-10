08de04c5f1e727cc1294e9ea4804b358924afbda
Rashmeen-Kaur <rashmeen.99@gmail.com>
2021-02-11 01:34:08 +0530

    Added HTML, CSS and functions to fetch different API's for filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import * as _ from 'underscore';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -13,36 +14,44 @@ export class AppComponent {
   spaceXList: any = [];
   launchYears: any = [];
   launchState: any;
-  launchesCount = 0;
-  constructor(private appService: AppService) { }
+  spaceXListCount = 0;
+  landState: string = "";
+  year: string = "";
+  constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit() {
     console.log("Hello");
 
     this.appService.getAllSpaceX().subscribe(
       res => {
-        //Prasing the response data
+        //Parsing the response res
         this.responseData = JSON.parse(JSON.stringify(res));
-        for (let i = 0; i < this.responseData.length; i++) {
-          var parsedData = JSON.parse(JSON.stringify(this.responseData[i]));
-          console.log("data of is , ", parsedData)
-          this.spaceXList.push(parsedData)
-        }
-        //Storing launch years in local array
-        var arr = this.spaceXList.map(function (data: any) {
-          return data.launch_year
-        })
-        //Removing duplicate years
-        this.launchYears = arr.filter(function (elem: any, index: any, self: any) {
-          return index === self.indexOf(elem);
-        })
-        //Sorting the years
-        this.launchYears.sort(
-          function (a: string, b: string) {
-            return parseInt(a) - parseInt(b)
+        if (this.responseData.length > 0) {
+          this.spaceXListCount = this.responseData.length;
+          for (let i = 0; i < this.responseData.length; i++) {
+            var parsedData = JSON.parse(JSON.stringify(this.responseData[i]));
+            console.log("res of is , ", parsedData)
+            this.spaceXList.push(parsedData)
           }
-        )
-        console.log("Years are ", this.launchYears);
+          //Storing launch years in local array
+          var arr = this.spaceXList.map(function (res: any) {
+            return res.launch_year
+          })
+          //Removing duplicate years
+          this.launchYears = arr.filter(function (elem: any, index: any, self: any) {
+            return index === self.indexOf(elem);
+          })
+          //Sorting the years
+          this.launchYears.sort(
+            function (a: string, b: string) {
+              return parseInt(a) - parseInt(b)
+            }
+          )
+          console.log("Years are ", this.launchYears);
+        } else {
+          this.spaceXListCount = 0;
+        }
+
       }, (err) => {
         console.log(err);
 
@@ -50,15 +59,70 @@ export class AppComponent {
     );
   }
 
+
+  // Filtering on the basis of launch_success
   filterLaunches(event: any) {
     this.launchState = event.target.textContent.toLowerCase();
-    this.appService.getSpaceXLaunch(this.launchState).subscribe((data) => {
-      this.spaceXList = data;
-      this.launchesCount = data.length;
+    this.appService.getSpaceXLaunch(this.launchState).subscribe((res) => {
+      this.spaceXList = res;
+      this.spaceXListCount = res.length;
     });
   }
 
-}
+  // Filtering on the basis of launch_year
+  filterYear(year: any) {
+    this.appService.getAllSpaceX().subscribe((res: any) => {
+      this.spaceXList = res.filter(function (elem: any) {
+        return elem.launch_year == year;
+      })
+    });
+  }
 
+
+  // Filtering on the basis of different conditions
+  filterLand(event: any) {
+    this.landState = event.target.textContent.toLowerCase();
+
+    if (this.launchState != "" && this.landState != "" && this.year == "") {
+      this.appService.getLaunchLand(this.launchState, this.landState).subscribe((res) => {
+        this.spaceXList = res;
+        this.spaceXListCount = res.length;
+      });
+    } else if (this.launchState != "" && this.landState != "" && this.year != "") {
+      this.appService.getAllSpaces(this.year, this.launchState, this.landState).subscribe((res: any) => {
+        this.spaceXList = res;
+        this.spaceXListCount = res.length;
+        return;
+      });
+    } else {
+      this.appService.getAllSpaceX().subscribe((res: any) => {
+        var land_status = this.landState;
+        this.spaceXList = res.filter(function (elem: any) {
+          return elem.rocket.first_stage.cores[0].land_success == land_status;
+        })
+        this.spaceXList = res;
+        this.spaceXListCount = res.length;
+        return;
+      });
+    }
+  }
+
+  // Resetting all the filters
+
+  resetFilters() {
+    this.appService.getAllSpaceX().subscribe((res: any) => {
+      this.spaceXList = [];
+      this.spaceXListCount = res.length;
+      for (let i = 0; i < this.responseData.length; i++) {
+        var parsedData = JSON.parse(JSON.stringify(this.responseData[i]));
+        console.log("res of is , ", parsedData)
+        this.spaceXList.push(parsedData)
+      }
+      return;
+    });
+  }
+
+
+}
 
 
